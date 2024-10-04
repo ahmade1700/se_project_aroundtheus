ee06779c9395696d037d971ccce3345241f93b12
Ahmad Lowe <Ahmade_usa@hotmail.com>
2024-10-04 12:43:58 -0400

    idk what im doing at this point

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 let initialCards = [
   {
-    Name: "Yosemite Valley",
-    link: https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg,
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
-    Name: "Lake Louise",
-    link: https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg,
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    Name: "Bald Mountains",
-    link: https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg,
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
-    Name: "Vanoise National Park",
-    link: https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg,
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    Name: "Latemar",
-    link: https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg,
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
-    Name: "Lago di Braies",
-    link: https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg,
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
