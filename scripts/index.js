3270af89caca02d3b592fd19f2e0c5119449e3ab
Ahmad Lowe <Ahmade_usa@hotmail.com>
2024-11-02 11:51:22 -0400

    fixed errors with the project. - Changed function names to be a verb instead of statement. - Moved all JS code into one file. - fixed minor design errors:     - Position of the exit button on the profile edit modal in mobile view shifted by 5%     - font size of the save button on the modal in desktop view set to 18px

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -27,6 +27,20 @@ let initialCards = [
 
 const cardsList = document.querySelector(".cards__list");
 
+const modal = document.querySelector(".modal");
+const profileForm = document.querySelector(".modal__form");
+
+const editButton = document.querySelector(".profile__edit-button");
+const exitButton = document.querySelector(".modal__exit");
+
+const profileName = document.querySelector(".profile__title");
+const profileDesc = document.querySelector(".profile__description");
+
+const nameEditInput = modal.querySelector("#title");
+const descEditInput = modal.querySelector("#description");
+
+const submitButton = profileForm.querySelector(".modal__button");
+
 let cardsTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
@@ -47,4 +61,35 @@ initialCards.forEach((cardData) => {
   const cardElement = getCardData(cardData);
 
   cardsList.prepend(cardElement);
+});
+
+// Functions
+
+function fillProfileInputs() {
+  nameEditInput.value = profileName.textContent;
+  descEditInput.value = profileDesc.textContent;
+}
+
+function setProfileData() {
+  profileName.textContent = nameEditInput.value;
+  profileDesc.textContent = descEditInput.value;
+}
+
+function toggleModal() {
+  modal.classList.toggle("modal-opened");
+}
+
+// Event Listners
+
+editButton.addEventListener("click", function () {
+  fillProfileInputs;
+  toggleModal();
+});
+
+exitButton.addEventListener("click", toggleModal);
+
+profileForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  setProfileData();
+  toggleModal();
 });
